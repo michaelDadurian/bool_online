aa36ad65f4b80879e7759edd023c908a9e6d0007
Michael Dadurian <mdadurian@gmail.com>
2017-05-08 17:43:58 -0400

    multiple owner search

--- src/main/java/com/bool/search/Search.java
+++ src/main/java/com/bool/search/Search.java
@@ -21,10 +21,12 @@ public class Search{
 
     private String query;
 
-    private String name;
-    private String tag;
-    private String owner;
-    private String shared;
+    private List<String> names = new ArrayList<String>();
+    private List<String> tags = new ArrayList<String>();
+    private List<String> owners = new ArrayList<String>();
+    private List<String> sharedWith = new ArrayList<String>();
+
+    boolean searchMultipleOwners = false;
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Datastore data = new Datastore();
@@ -43,10 +45,6 @@ public class Search{
         * tokenize on space, if there is something unrecognizable then just filter on name
         * "Nelson's Circuit" use contains filter to search for any name containing Nelson's and Circuit
         * */
-        List<String> names = new ArrayList<String>();
-        List<String> tags = new ArrayList<String>();
-        List<String> owners = new ArrayList<String>();
-        List<String> sharedWith = new ArrayList<String>();
 
         String[] splitQuery = query.split(" ");
 
@@ -73,6 +71,109 @@ public class Search{
 
         }
 
+        List<Entity> searchResults = new ArrayList<>();
+
+        if (owners.size() > 1){
+            for (String owner: owners){
+                List<Entity> ownerSearch = multipleOwnersSearch(owner, defaultFilter, tags, sharedWith, names);
+                searchResults.addAll(ownerSearch);
+            }
+        }else{
+            searchResults = getSearchResults(defaultFilter, tags, sharedWith, owners, names);
+        }
+
+
+
+
+        return searchResults;
+
+
+
+    }
+
+    public List<Entity> multipleOwnersSearch(String owner, List<Entity> defaultFilter, List<String> tags, List<String> sharedWith, List<String> names){
+        List<Entity> searchResult = new ArrayList<>();
+
+        for (Entity circuit: defaultFilter){
+            String currTags = (String)circuit.getProperty("tags");
+            String currShared = (String)circuit.getProperty("shared");
+            String currOwner = (String)circuit.getProperty("owner");
+            String currName = (String)circuit.getProperty("name");
+
+            if (owner.equals(currOwner)){
+                /*Search for circuits by different owners*/
+                if (tags.isEmpty() && sharedWith.isEmpty() && names.isEmpty()){
+                    if (!searchResult.contains(circuit))
+                        searchResult.add(circuit);
+                }
+
+                /*Search for circuit by owner and tag*/
+                else if (!tags.isEmpty() && sharedWith.isEmpty() && names.isEmpty()){
+                    if (checkProperty(currTags, tags)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+
+                /*Search by owner and shared*/
+                else if (tags.isEmpty() && !sharedWith.isEmpty() && names.isEmpty()){
+                    if (checkProperty(currShared, sharedWith)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+
+                /*Search by owner and circuit name*/
+                else if (tags.isEmpty() && sharedWith.isEmpty() && !names.isEmpty()){
+                    if (checkProperty(currName, names)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+
+                /*Search by owner, tags, and name*/
+                else if (!tags.isEmpty() && sharedWith.isEmpty() && !names.isEmpty()){
+                    if (checkProperty(currTags, tags) && checkProperty(currName, names)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+                /*Search by owner, tag, and shared*/
+                else if (!tags.isEmpty() && !sharedWith.isEmpty() && names.isEmpty()){
+                    if (checkProperty(currTags, tags) && checkProperty(currShared, sharedWith)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+
+                /*Search by owner, shared, and name*/
+                else if (tags.isEmpty() && !sharedWith.isEmpty() && !names.isEmpty()){
+                    if (checkProperty(currShared, sharedWith) && checkProperty(currName, names)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+
+                /*Search by all 4*/
+                else if (!tags.isEmpty() && !sharedWith.isEmpty() && !names.isEmpty()){
+                    if (checkProperty(currTags, tags) && checkProperty(currShared, sharedWith) && checkProperty(currName, names)){
+                        if (!searchResult.contains(circuit))
+                            searchResult.add(circuit);
+                    }
+                }
+            }
+
+
+
+        }
+
+
+        return searchResult;
+
+    }
+
+    public List<Entity> getSearchResults(List<Entity> defaultFilter, List<String> tags, List<String> sharedWith, List<String> owners, List<String> names){
+
         List<Entity> searchResult = new ArrayList<>();
         for (Entity circuit: defaultFilter){
             String currTags = (String)circuit.getProperty("tags");
@@ -80,10 +181,10 @@ public class Search{
             String currOwner = (String)circuit.getProperty("owner");
             String currName = (String)circuit.getProperty("name");
 
-            //System.out.println("Curr entity: " + circuit.getProperty("name"));
+
             /*Search by tag only*/
             if (!tags.isEmpty() && sharedWith.isEmpty() && owners.isEmpty() && names.isEmpty()){
-                System.out.println("Search by tag");
+
                 if (checkProperty(currTags, tags)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
@@ -91,7 +192,7 @@ public class Search{
             }
             /*Search by tag and shared with*/
             else if (!tags.isEmpty() && !sharedWith.isEmpty() && owners.isEmpty() && names.isEmpty()){
-                System.out.println("Search by tag and shared");
+
                 if (checkProperty(currTags, tags) && checkProperty(currShared, sharedWith)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
@@ -100,7 +201,7 @@ public class Search{
 
             /*Search by tag and shared with and owners*/
             else if (!tags.isEmpty() && !sharedWith.isEmpty() && !owners.isEmpty() && names.isEmpty()){
-                System.out.println("Search by tag, shared, and owner");
+
                 if (checkProperty(currTags, tags) && checkProperty(currShared, sharedWith) && checkProperty(currOwner, owners)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
@@ -109,41 +210,55 @@ public class Search{
 
             /*Search by tag and shared with and owners and name*/
             else if (!tags.isEmpty() && !sharedWith.isEmpty() && !owners.isEmpty() && !names.isEmpty()){
-                System.out.println("Search by tag, shared, owner, and name");
-                
+
+
                 if (checkProperty(currTags, tags) && checkProperty(currShared, sharedWith) && checkProperty(currOwner, owners) && checkProperty(currName, names)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
                 }
             }
 
+            /*Search by owner*/
             else if (tags.isEmpty() && sharedWith.isEmpty() && !owners.isEmpty() && names.isEmpty()){
-                System.out.println("Search by owner");
+
                 if (checkProperty(currOwner, owners)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
                 }
 
             }
+
+            /*Search by name*/
             else if (tags.isEmpty() && sharedWith.isEmpty() && owners.isEmpty() && !names.isEmpty()){
-                System.out.println("Search by name");
+
                 if (checkProperty(currName, names)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
                 }
 
             }
+
+            /*Search by tag and name*/
             else if (!tags.isEmpty() && sharedWith.isEmpty() && owners.isEmpty() && !names.isEmpty()){
-                System.out.println("Search by name and tags");
+
                 if (checkProperty(currName, names) && checkProperty(currTags, tags)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
                 }
             }
 
+            /*Search by shared*/
             else if (tags.isEmpty() && !sharedWith.isEmpty() && owners.isEmpty() && names.isEmpty()){
-                System.out.println("Search by shared");
+
                 if (checkProperty(currShared, sharedWith)){
+                    if (!searchResult.contains(circuit))
+                        searchResult.add(circuit);
+                }
+            }
+
+            /*Search by owner and tag*/
+            else if (!tags.isEmpty() && sharedWith.isEmpty() && !owners.isEmpty() && names.isEmpty()){
+                if (checkProperty(currTags, tags) && checkProperty(currOwner, owners)){
                     if (!searchResult.contains(circuit))
                         searchResult.add(circuit);
                 }
@@ -155,7 +270,6 @@ public class Search{
         }
 
         return searchResult;
-
 
     }
 
